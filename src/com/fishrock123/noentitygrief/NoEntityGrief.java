3487eaaad3debcd42cbee458316e239296924b4a
Malte Renken <malte.renken@wwu.de>
2013-02-24 01:12:23 +0100

    Inverted config file.

--- src/com/fishrock123/noentitygrief/NoEntityGrief.java
+++ src/com/fishrock123/noentitygrief/NoEntityGrief.java
@@ -1,4 +1,5 @@
 package com.fishrock123.noentitygrief;
+
 import java.util.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.*;
@@ -6,9 +7,15 @@ import org.bukkit.event.block.*;
 import org.bukkit.event.entity.*;
 import org.bukkit.event.hanging.*;
 import org.bukkit.plugin.java.JavaPlugin;
-public class NoEntityGrief extends JavaPlugin implements Listener { // Licensed under GPLv3
-	@SuppressWarnings("unchecked") private List<String>[] EWs = new ArrayList[6];
-	@Override public void onEnable() {
+
+public class NoEntityGrief extends JavaPlugin implements Listener { // Licensed
+																	// under
+																	// GPLv3
+	@SuppressWarnings("unchecked")
+	private List<String>[] EWs = new ArrayList[6];
+
+	@Override
+	public void onEnable() {
 		if (getConfig().options().header() == null) {
 			getConfig().options().copyHeader();
 			getConfig().options().copyDefaults(true);
@@ -22,25 +29,45 @@ public class NoEntityGrief extends JavaPlugin implements Listener { // Licensed 
 		EWs[5] = getConfig().getStringList("Silverfish");
 		getServer().getPluginManager().registerEvents(this, this);
 	}
-	@EventHandler public void onBlockChange(EntityChangeBlockEvent e) {
-		if (e.getEntity() instanceof Enderman && (EWs[0] == null || !EWs[0].contains(e.getBlock().getLocation().getWorld().getName()))) e.setCancelled(true);
-		else if (e.getEntity() instanceof Silverfish && (EWs[5] == null || !EWs[5].contains(e.getBlock().getLocation().getWorld().getName()))) e.setCancelled(true);
-		else if (e.getEntity() instanceof Sheep && (EWs[1] == null || !EWs[1].contains(e.getBlock().getLocation().getWorld().getName()))) {
+
+	@EventHandler
+	public void onBlockChange(EntityChangeBlockEvent e) {
+		if (e.getEntity() instanceof Enderman
+				&& EWs[0] != null && EWs[0].contains(e.getBlock().getLocation().getWorld().getName()))
 			e.setCancelled(true);
-			((Sheep)e.getEntity()).setSheared(false);
+		else if (e.getEntity() instanceof Silverfish
+				&& EWs[5] != null && EWs[5].contains(e.getBlock().getLocation().getWorld().getName()))
+			e.setCancelled(true);
+		else if (e.getEntity() instanceof Sheep
+				&& EWs[1] != null && EWs[1].contains(e.getBlock().getLocation().getWorld().getName())) {
+			e.setCancelled(true);
+			((Sheep) e.getEntity()).setSheared(false);
 		}
 	}
-	@EventHandler public void onExplosion(EntityExplodeEvent e) {
-		if (e.getEntity() instanceof TNTPrimed) { 
-			if (EWs[3] == null || !EWs[3].contains(e.getLocation().getWorld().getName())) e.blockList().clear();
-		} else if (EWs[2] == null || !EWs[2].contains(e.getLocation().getWorld().getName())) e.blockList().clear();
+
+	@EventHandler
+	public void onExplosion(EntityExplodeEvent e) {
+		if (e.getEntity() instanceof TNTPrimed) {
+			if (EWs[3] != null && EWs[3].contains(e.getLocation().getWorld().getName()))
+				e.blockList().clear();
+		} else if (EWs[2] != null && EWs[2].contains(e.getLocation().getWorld().getName()))
+			e.blockList().clear();
 	}
-	@EventHandler public void onPaintingBreak(HangingBreakEvent e) {
-		if (e.getCause() == HangingBreakEvent.RemoveCause.EXPLOSION && e.getEntity() instanceof TNTPrimed) { 
-			if (EWs[3] == null || !EWs[3].contains(e.getEntity().getWorld().getName())) e.setCancelled(true);
-		} else if (e.getCause() == HangingBreakEvent.RemoveCause.EXPLOSION && (EWs[2] == null || !EWs[2].contains(e.getEntity().getWorld().getName()))) e.setCancelled(true);
+
+	@EventHandler
+	public void onPaintingBreak(HangingBreakEvent e) {
+		if (e.getCause() == HangingBreakEvent.RemoveCause.EXPLOSION
+				&& e.getEntity() instanceof TNTPrimed) {
+			if (EWs[3] != null && EWs[3].contains(e.getEntity().getWorld().getName()))
+				e.setCancelled(true);
+		} else if (e.getCause() == HangingBreakEvent.RemoveCause.EXPLOSION
+				&& EWs[2] != null && EWs[2].contains(e.getEntity().getWorld().getName()))
+			e.setCancelled(true);
 	}
-	@EventHandler public void onEntityForm(EntityBlockFormEvent e) {
-		if (EWs[4] == null || !EWs[4].contains(e.getBlock().getLocation().getWorld().getName())) e.setCancelled(true);
+
+	@EventHandler
+	public void onEntityForm(EntityBlockFormEvent e) {
+		if (EWs[4] != null && EWs[4].contains(e.getBlock().getLocation().getWorld().getName()))
+			e.setCancelled(true);
 	}
 }
